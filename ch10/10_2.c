8c8a8efa0fc4fdb15172852a9ede0669cb63752a
JIElite <ita3051@gmail.com>
2015-03-28 22:29:06 +0800

    modify 10_2.c

--- ch10/10_2.c
+++ ch10/10_2.c
@@ -13,7 +13,8 @@ int main(){
 	 * char *apple[4] 是有四個元素，每個元素都是指向char的指標型態
 	 * char (*pea)[4] pea 則是一個指標，只是指向的東西是 char [4] 這種東西
 	 * 
-	 * 所以 type of pea : char *[4]
+	 * 所以 type of apple : char *[4]
+	 * 	type of pea   : char (*)[4]
 	 *
 	 */
 }
